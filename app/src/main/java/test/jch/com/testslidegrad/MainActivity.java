40aabf0cd925465a47c9291e1caf49e613e8ecf1
wangjingbiao <wwwwjb123@126.com>
2014-11-08 12:22:39 +0800

    fds

--- app/src/main/java/test/jch/com/testslidegrad/MainActivity.java
+++ app/src/main/java/test/jch/com/testslidegrad/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialize();
+
+
+
+
     }
 
     /**
